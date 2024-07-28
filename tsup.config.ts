fe36664e3c15d706af45a9ef7048eb9d6afe76de
Andrew Benington <andrewbenington57@gmail.com>
2024-07-28 13:15:10 -0500

    add moves

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
-  dts: {
-    compilerOptions: {
-        moduleResolution: "node"
-    }
-  }, // Generate declaration file (.d.ts)
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  bundle: true
-});
+    entry: ['src/index.ts'],
+    format: ['cjs', 'esm'], // Build for commonJS and ESmodules
+    dts: {
+        compilerOptions: {
+            moduleResolution: 'node',
+            resolveJsonModule: true,
+            allowSyntheticDefaultImports: true,
+        },
+    }, // Generate declaration file (.d.ts)
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    bundle: true,
+})
